65bbcdc0403e3f7861f3f0e069ec5b51bd9b032d
Bethala Suresh <bethalasuresh@gmail.com>
2025-03-04 11:01:58 +0530

    Update PushNotification.java

--- HeadFirstDP/src/com/suresh/designprinciples/ocp/problem/PushNotification.java
+++ HeadFirstDP/src/com/suresh/designprinciples/ocp/problem/PushNotification.java
@@ -1,3 +1,5 @@
+package com.suresh.designprinciples.ocp.problem
+
 public class PushNotification {
   public void send(String message) {
     System.out.println("Sending Push Notification" + message);
