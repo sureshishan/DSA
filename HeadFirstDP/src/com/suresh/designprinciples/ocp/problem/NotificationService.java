81c266256f88e248fcc2fa6f11be40bdeb85c37a
Bethala Suresh <bethalasuresh@gmail.com>
2025-03-04 11:05:25 +0530

    Update NotificationService.java

--- HeadFirstDP/src/com/suresh/designprinciples/ocp/problem/NotificationService.java
+++ HeadFirstDP/src/com/suresh/designprinciples/ocp/problem/NotificationService.java
@@ -3,10 +3,13 @@ package com.suresh.designprinciples.ocp.problem;
 public class NotificationService {
     public void sendNotification(String type, String message) {
         if (type.equalsIgnoreCase("EMAIL"))
-            PushNotification pushNotification = new PushNotification();
-            pushNotification.send(message);
-            System.out.println("Sending EMAIL: " + message);
+            EmailNotification emailNotification = new EmailNotification();
+            emailNotification.send(message);
         else if (type.equalsIgnoreCase("SMS")) {
+            SmsNotification smsNotification = new SmsNotification();
+            pushNotification.send(message);
+        } else if (type.equalsIgnoreCase("PUSH")) {
+            PushNotification pushNotification = new PushNotification();
             System.out.println("Sending SMS: " + message);
         }
     }
